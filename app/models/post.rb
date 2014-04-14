8f44ab9152df384e9645130cdea1d5f431f15ccc
Alex Peattie <alexpeattie@gmail.com>
2014-04-14 11:28:40 +0100

    Upgrade to paperclip 4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ActiveRecord::Base
-  validates :description, presence: true
-  # validates_attachment_content_type :picture, content_type: ["image/jpg", "image/jpeg", "image/png"]
-
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
 
+  validates :description, presence: true
+  validates_attachment_content_type :picture, content_type: ["image/jpg", "image/jpeg", "image/png"]
 end
